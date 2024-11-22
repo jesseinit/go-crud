b56e6e169575410a5d5674ad16ece3c44cde6cc7
Jesse Egbosionu <jesse.egbosionu@finqle.com>
2024-11-22 06:46:57 +0100

    feat: added logs on server starting

--- src/main.go
+++ src/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -17,7 +19,7 @@ type Book struct {
 	ID        string   `json:"id,omitempty"`
 	Title     string   `json:"title,omitempty"`
 	Author    string   `json:"author,omitempty"`
-	Publisher *Company `json:"publisher,omitempty"`
+	Publisher *Company `json:"publisher"`
 }
 
 var books []Book
@@ -83,16 +85,24 @@ func main() {
 		Publisher: &Company{Name: "Company One", Address: "Address One"}})
 	books = append(books, Book{
 		ID: "2", Title: "Book Two", Author: "Jane Doe",
-		Publisher: &Company{Name: "Company Two", Address: "Address Two"}})
+	})
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World!")
 	})
+
 	router.HandleFunc("/books", GetBooks).Methods(http.MethodGet)
 	router.HandleFunc("/books{id}", GetBook).Methods(http.MethodGet)
 	router.HandleFunc("/books", CreateBook).Methods(http.MethodPost)
 	router.HandleFunc("/books{id}", UpdateBook).Methods(http.MethodPut)
 	router.HandleFunc("/books{id}", DeleteBook).Methods(http.MethodDelete)
 
-	http.ListenAndServe(":8082", router)
+	PORT := os.Getenv("SERVER_PORT")
+	if PORT == "" {
+		PORT = ":8082"
+	}
+	log.Printf("Starting server on %v", PORT)
+	if err := http.ListenAndServe(PORT, router); err != nil {
+		log.Fatalf("Server failed to start on %v", PORT)
+	}
 }
